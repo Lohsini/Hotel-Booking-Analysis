fa222825578fb4ea25265112b1c8feef3a107b87
Lohisni <lohi3152@gmail.com>
2024-05-03 04:53:50 -0700

    add k-anonymity

--- addDatainCVS.py
+++ addDatainCVS.py
@@ -1,23 +1,25 @@
 import csv
-import random
+import numpy as np  # type: ignore
+import shutil
 
-with open('hotel_booking.csv', 'r', newline='', encoding='utf-8') as csvfile, \
-     open('temp.csv', 'w', newline='', encoding='utf-8') as temp_csvfile:
-    
+mu = 45
+sigma = 15
+
+with open('./modified_data/hotel_booking_simple.csv', 'r', newline='', encoding='utf-8') as csvfile, \
+        open('temp.csv', 'w', newline='', encoding='utf-8') as temp_csvfile:
+
     # create reader and writer
     csvreader = csv.reader(csvfile)
     csvwriter = csv.writer(temp_csvfile)
-    
-    header = next(csvreader)    
+
+    header = next(csvreader)
     header.append('age')
     csvwriter.writerow(header)
-    
+
     for row in csvreader:
-        age = random.randint(17, 90)
+        age = int(np.random.normal(mu, sigma))
+        age = max(min(age, 90), 17)
         row.append(age)
         csvwriter.writerow(row)
-import os
-os.rename('temp.csv', 'hotel_booking.csv')
 
-
-
+shutil.move('temp.csv', './modified_data/hotel_booking_simple(+age).csv')
